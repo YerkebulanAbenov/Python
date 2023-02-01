5475477b53aa32f9901147c1cb39c15c6840acc9
YerkebulanAbenov <118619322+YerkebulanAbenov@users.noreply.github.com>
2023-02-01 21:17:06 +0600

    Add files via upload

---
+++ lesson_1.py
@@ -0,0 +1,2 @@
+n = int(input("Please insert number => "))
+print(int(n),int(n*2),int(n*3))
---
+++ lesson_3.py
@@ -0,0 +1,4 @@
+name = input("Please insert your name => ")
+password = input ("Please insert your password => ")
+age = input ("Please insert your age => ")
+print("Your name is " + name + ", your password is " + password + ", you are " + age + " years old")
